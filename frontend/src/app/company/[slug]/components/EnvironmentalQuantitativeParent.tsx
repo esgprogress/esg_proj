d7b87d2be04e0b9897d4d7432502d57b33db6278
Arjun113 <arjun@quickwork.co>
2026-02-12 16:23:24 +0530

    fix: add bg to button

--- frontend/src/app/company/[slug]/components/EnvironmentalQuantitativeParent.tsx
+++ frontend/src/app/company/[slug]/components/EnvironmentalQuantitativeParent.tsx
@@ -34,7 +34,8 @@ export default function EnvironmentalQuantitativeParent({company}: Environmental
 
             <div className="fixed bottom-6 right-6 z-50">
                 <Button
-                    className="h-auto px-5 py-3 rounded-full shadow-lg whitespace-nowrap"
+                    className="h-auto px-5 py-3 rounded-full shadow-lg whitespace-nowrap
+                   bg-[#c6d1b3] text-black hover:bg-[#b7c3a4]"
                     onClick={swapTextFieldValues}
                 >
                     {text}
